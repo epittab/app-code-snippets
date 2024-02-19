749b9efb436fbcf27840a2e289e9b91092eefb4a
Enrique Pittaluga <ep7m@me.com>
2024-02-18 20:26:49 -0500

    testing different declaration pattern

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const handler = async (event, context) => {
+exports.handler = async (event, context) => {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     return context.logStreamName;
   };
