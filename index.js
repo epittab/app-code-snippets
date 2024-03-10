2fbfa42d33d3dd700ca340ba9437cf0709b1b0f8
Enrique Pittaluga <ep7m@me.com>
2024-03-09 22:36:02 -0500

    moving all code into one file

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-import { createUser } from "./user";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+let client = null;
+
+export const getClient = () => {
+    if (client) return client;
+    client = new DynamoDB({region: "us-east-1"});
+    return client;
+};
+
+const pk = (value = "") => `USER#${value}`;
+
+const sk = (value = "") => `USER#${value}`;
+
+export const createUser = async (user = {}) => {
+    let client = getClient();
+
+    let data = {
+        "PK": {"S": pk(user.name)},
+        "SK": {"S": sk(user.team)},
+        "name": {"S": user.name},
+        "age": {"N": user.age},
+        "favoriteTeam": {"S": user.team},
+    };
+
+    const params = {
+        TableName: "app-db",
+        Item: data
+    };
+
+    client.putItems(params, (e, d) => {
+        if (e) {
+            console.error(e);
+        } else {
+            console.log("This was returned from ddb");
+            console.log(d);
+        }
+    })
+
+}
+
 
 exports.handler = async (event, context) => {
-    console.log("Starting invocation");
+  console.log("Starting invocation");
 
-    if (event["action"] === "create") {
-      console.log("Creating user");
-      let user = event["user"]
-      createUser(user);
-    }
-    
+  if (event["action"] === "create") {
+    console.log("Creating user");
+    let user = event["user"]
+    await createUser(user);
+  }
+  
 
-    return context.logStreamName;
-  };
+  return context.logStreamName;
+};
